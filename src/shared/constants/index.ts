04a41a5951d84b534c404e930f9e4dadf51de63a
Valentyn13 <valentyn13git@gmail.com>
2024-06-02 18:53:13 +0300

    initial commit

---
+++ src/shared/constants/index.ts
@@ -0,0 +1,14 @@
+export {
+    ApiEndpoints,
+    ContentType,
+    CookieName,
+    HttpHeader,
+    StorageKey,
+} from './enums/enums.ts';
+export {
+    type HttpApiOptions,
+    type HttpApiResponse,
+    type HttpMethod,
+    type HttpOptions,
+    type ValueOf,
+} from './types/types.ts';
