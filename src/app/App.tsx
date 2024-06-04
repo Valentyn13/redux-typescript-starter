1eeb67d5638cdf95a6148dd6a518c7e6e06e67ed
Valentyn13 <valentyn13git@gmail.com>
2024-06-04 11:53:54 +0300

    chore: test commit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -12,7 +12,7 @@ const App = () => {
     };
     return (
         <>
-            <h1>Hello world</h1>
+            <h1>Hello world!!!</h1>
             <button onClick={handleClick}>get data</button>
             {data && <p>{JSON.stringify(data)}</p>}
         </>
